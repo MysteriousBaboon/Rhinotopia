645eccb7042c9b365f30932e03a48475ff012e05
MysteriouBaboon <MysteriousBaboon@hotmail.com>
2020-06-02 20:02:50 +0200

    Change size of race

--- rpg/models.py
+++ rpg/models.py
@@ -71,7 +71,7 @@ class Character(models.Model):
                               default=Species.RHINOCEROS, )
 
     # Race of the character
-    race = models.CharField(max_length=15, default='Unspecified')
+    race = models.CharField(max_length=20, default='Unspecified')
     evolution_level = models.IntegerField(default=0)
     can_Evolve = models.BooleanField(default=True)
 
