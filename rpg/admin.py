a2827db3dc516ed587c3127eeca6017669b285f1
MysteriouBaboon <MysteriousBaboon@hotmail.com>
2020-06-02 20:18:35 +0200

    Added profile to admin

--- rpg/admin.py
+++ rpg/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Character, Mission
+from .models import Character, Mission, Profile
 
 
 admin.site.register(Character)
 admin.site.register(Mission)
+admin.site.register(Profile)
